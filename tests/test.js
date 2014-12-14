75e5577d6dd148eb4cd6a4802ea8ab90c74e69b2
TRex22 <jasonchalom@yahoo.co.uk>
2014-12-14 15:19:14 +0200

    some slight changes

--- tests/test.js
+++ tests/test.js
@@ -2,11 +2,9 @@
   Under the MIT License
   This will take some test json data and convert to xlsx using desired styling and formatting
   https://github.com/natergj/excel4node
-
-  **There are some todos
 */
 "use strict";
-var xl_xp = require('./excel_export.js');
+var xl_xp = require('../excel_export.js');
 var fs = require('fs');
 
 var config = JSON.parse(fs.readFileSync("config.json"));
