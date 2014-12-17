54fc05fc4f2b97bc744187049d3dacd6d239d3d0
jason.chalom <jason.chalom@entelect.co.za>
2014-12-17 16:32:32 +0200

    added unit testing framework and some small code improvements

--- test/index.js
+++ test/index.js
@@ -1,48 +1,28 @@
-// var should = require('chai').should(),
-// scapegoat = require('../lib/excel_export.js'),
-// escape = scapegoat.escape,
-// unescape = scapegoat.unescape;
-//
-// describe('#escape', function() {
-//   it('converts & into &amp;', function() {
-//     escape('&').should.equal('&amp;');
-//   });
-//
-//   it('converts " into &quot;', function() {
-//     escape('"').should.equal('&quot;');
-//   });
-//
-//   it('converts \' into &#39;', function() {
-//     escape('\'').should.equal('&#39;');
-//   });
-//
-//   it('converts < into &lt;', function() {
-//     escape('<').should.equal('&lt;');
-//   });
-//
-//   it('converts > into &gt;', function() {
-//     escape('>').should.equal('&gt;');
-//   });
-// });
-//
-// describe('#unescape', function() {
-//   it('converts &amp; into &', function() {
-//     unescape('&amp;').should.equal('&');
-//   });
-//
-//   it('converts &quot; into "', function() {
-//     unescape('&quot;').should.equal('"');
-//   });
-//
-//   it('converts &#39; into \'', function() {
-//     unescape('&#39;').should.equal('\'');
-//   });
-//
-//   it('converts &lt; into <', function() {
-//     unescape('&lt;').should.equal('<');
-//   });
-//
-//   it('converts &gt; into >', function() {
-//     unescape('&gt;').should.equal('>');
-//   });
-// });
+var should = require('chai').should();
+//var assert = require("assert");
+
+var fs = require('fs');
+var xl = require('excel4node');
+var util = require('util');
+
+var fn = require('./customtestFunctions.js');
+
+/*js to test*/
+var xl_xp = require('../lib/excel_export.js');
+var helper = require('../lib/helpers.js');
+var mrData = require('../lib/mrData.js');
+var stylizer = require('../lib/stylizer.js');
+
+var config = JSON.parse(fs.readFileSync("test/test_config.json"));
+var reports = JSON.parse(fs.readFileSync("" + config.dir + config.reportsFile));
+var styleObj = JSON.parse(fs.readFileSync("" + config.dir + config.stylesFile));
+
+describe('#xl_xp', function() {
+  // it('createReports', function(){
+  //   config.internalWbLibDebug = false;
+  //   config.debug = false;
+  //   config.fileWriter = true;
+  //   xl_xp.createReports(reports, styleObj, config);
+  // });
+
+});
