6a849b4fc973d6cb1c5b17152c99662520f1ad03
Jason Chalom <jasonchalom@yahoo.co.uk>
2014-12-27 15:32:47 +0000

    building csv parser

---
+++ test/parser_test.js
@@ -0,0 +1,15 @@
+/*Unit tests for parsers, may use external dependencies*/
+var should = require('chai').should();
+
+var fs = require('fs');
+var xl = require('excel4node');
+var util = require('util');
+
+var helper = require('../lib/helpers.js');
+var csvParser = require('../lib/parser/csv.js');
+
+describe('#csvParser', function() {
+   it('csvParser.open', function(){
+     
+   });
+});
