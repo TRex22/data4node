4689bdc4ce80a35933e722444745d8e35fad3337
unknown <jason.chalom@JasonC-PC.entelect.local>
2015-01-31 17:28:07 +0200

    added obj to html

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ var util = require('util');
 
 
 /*js to test*/
-var xl_xp = require('../lib/data4node.js');
+var data4node = require('../lib/data4node.js');
 var helper = require('../lib/helpers.js');
 var mrData = require('../lib/mrData/mrData.js');
 var stylizer = require('../lib/excel_export/stylizer.js');
@@ -21,7 +21,20 @@ describe('#xl_xp', function() {
     config.internalWbLibDebug = false;
     config.debug = false;
     config.fileWriter = false; //todo make a report and run a check on it
-    xl_xp.createExcelReports(reports, styleObj, config);
+    data4node.createExcelReports(reports, styleObj, config);
   });
 
+  it('createHtmlFromJson', function(){
+    var data =
+    {
+      t1: "hello",
+      t2: {
+        h1: "bye",
+        h2: "225552",
+        H3: 1337
+      }
+    };
+    var testOut = data4node.createHtmlListFromJson(data);
+    console.log(testOut);
+  });
 });
