1a7f74d7a411580eddf7c15c226e96e618574f3a
TRex22 <contact@jasonchalom.com>
2015-12-28 02:00:38 +0200

    attempting auto deploy

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo 'Increment version number'
+echo 'will not push to master only develop, master inherits version from latest merged develop'
 npm version patch
+git add --all
+git commit -am "[ci skip] prod number increment"
+git push origin develop
